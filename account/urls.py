63b107afae9f4190a9abc17c80d82f3ce51e11f3
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2021-01-31 23:18:49 +0300

    Create user registration form

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path("login/", views.user_login, name="login"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("register/", views.register, name="register"),
     # Change password urls
     path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
     path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
