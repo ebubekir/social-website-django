c55894aa4d039319608b19e8d9f97e0624f916bd
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2021-02-04 17:02:56 +0300

    Email authentication

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -38,6 +38,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
 ROOT_URLCONF = 'bookmarks.urls'
 
 TEMPLATES = [
