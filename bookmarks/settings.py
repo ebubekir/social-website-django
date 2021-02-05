187ee43304d980d97acb90624cb11805d182626f
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2021-02-05 11:02:55 +0300

    Create images app

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = ['mysite.com','localhost','127.0.0.1']
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    'images.apps.ImagesConfig',
     'sslserver',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -28,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    
 ]
 
 MIDDLEWARE = [
